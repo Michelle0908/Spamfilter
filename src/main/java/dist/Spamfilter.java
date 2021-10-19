534ffa027cace33ffb71108dcfa51037bea58059
Vonesch Tobias (s) <tobias.vonesch@students.fhnw.ch>
2021-10-19 19:02:33 +0200

    ref little bit more structure

--- src/main/java/dist/Spamfilter.java
+++ src/main/java/dist/Spamfilter.java
@@ -2,6 +2,7 @@ package dist;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
@@ -34,24 +35,37 @@ public class Spamfilter {
 
     public static void main(String[] args) {
 
+        /***********************************
+        Read All Files in the folders and store them in an Array of File Objects
+        ************************************/
 
         readAllFiles();
+
+        /***********************************
+        Learning Process:
+        Go Through all the Files/Mails in the folder, read them as Strings, chop up the Strings, export all distinct
+        words as a Set, look up words and increase the count in the hashmap if the word has already appeared or add them
+        to the hashmap and last but not least, increment the right count to keep track on how many files have been
+        analysed.
+        ************************************/
+
         spamMailsCount  = learnWordsFromAFolder(listOfSpamLearningFiles,   spamWords);
         hamMailsCount   = learnWordsFromAFolder(listOfHamLearningFiles,    hamWords);
-        System.out.println(spamMailsCount);
-        System.out.println(hamMailsCount);
-        System.out.println(spamWords);
-//        System.out.println(hamWords);
-        //
 
+        System.out.println("The End");
         /*
-        Test if gets read correctly
+        Test Space
          */
 //        File testFile = new File(FILE_ROOT_PATH + HAM_MAILS_LEARNING + "0126.d002ec3f8a9aff31258bf03d62abdafa");
 //        String input = readFromTextFile(testFile);
 //        input = input.replaceAll("<[^>]*>|;", "");
 //        Set<String> output = split(input);
 //        System.out.println(output);
+
+//        System.out.println(spamMailsCount);
+//        System.out.println(hamMailsCount);
+//        System.out.println(spamWords);
+//        System.out.println(hamWords);
 
     }
 
@@ -105,7 +119,7 @@ public class Spamfilter {
 
     public static String readFromTextFile(File file){
         try {
-            return Files.readString(file.toPath());
+            return Files.readString(file.toPath(), StandardCharsets.UTF_8);
         } catch (IOException e){
             e.printStackTrace();
         }
