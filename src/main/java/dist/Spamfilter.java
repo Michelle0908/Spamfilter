4793b2272176257cba37fe63c7f740a42bb91412
Vonesch Tobias (s) <tobias.vonesch@students.fhnw.ch>
2021-10-19 15:08:24 +0200

    add final path variables and hashMaps

--- src/main/java/dist/Spamfilter.java
+++ src/main/java/dist/Spamfilter.java
@@ -1,8 +1,25 @@
 package dist;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Spamfilter {
+
+    private final static String     FILE_ROOT_PATH          = "src/main/files/";
+    private final static String     HAM_MAILS_LEARNING      = "ham-anlern";
+    private final static String     SPAM_MAILS_LEARNING     = "spam-anlern";
+    private final static String     HAM_MAILS_CALIBRATION   = "ham-kallibrierung";
+    private final static String     SPAM_MAILS_CALIBRATION  = "spam-kallibrierung";
+    private final static String     HAM_MAILS_TEST          = "ham-test";
+    private final static String     SPAM_MAILS_TEST         = "spam-test";
+
+    private Map<String, Integer>    spamWords               = new HashMap<>();
+    private Map<String, Integer>    hamWords                = new HashMap<>();
+    private int                     spamCount               = 0;
+    private int                     hamCount                = 0;
 
     public static void main(String[] args) {
         System.out.println("Hej Michelle, nice you're here!");
     }
+
 }
